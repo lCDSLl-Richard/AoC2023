b6d89b0fc2da391e6acc0c0d91d2bd3ddd129394
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-12-04 16:44:42 -0600

    Solved day 1 part 1 AoC 2023

---
+++ day1/part1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+  if (argc != 2)
+  {
+    cout << "Invalid arguments. Usage ./app <filename>\n";
+    return 1;
+  }
+
+  ifstream inFile(argv[1]);
+
+  if (!inFile.is_open())
+  {
+    cout << "Could not open " << argv[1] << ".\n";
+    return 1;
+  }
+
+  map<string, int> nNames = {{"one", 1},
+                             {"two", 2},
+                             {"three", 3},
+                             {"four", 4},
+                             {"five", 5},
+                             {"six", 6},
+                             {"seven", 7},
+                             {"eight", 8},
+                             {"nine", 9}};
+
+  string line;
+  inFile >> line;
+  string::iterator left = line.begin();
+  string::iterator right = line.end();
+  int sum = 0;
+
+  while (inFile)
+  {
+    while (!isdigit(*left) || !isdigit(*right))
+    {
+      if (!isdigit(*left))
+        left++;
+
+      if (!isdigit(*right))
+        right--;
+    }
+
+    cout << *left << " " << *right << "\n";
+
+    sum += ((int)*left - 48) * 10 + (int)*right - 48;
+
+    inFile >> line;
+    left = line.begin();
+    right = line.end();
+  }
+
+  cout << "Final sum: " << sum << "\n";
+  inFile.close();
+
+  return 0;
+}
